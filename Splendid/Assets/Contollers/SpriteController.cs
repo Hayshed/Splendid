d584007f9089fb484ea9821cd07fc04caa9be9eb
Hayshed <44012480+Hayshed@users.noreply.github.com>
2018-10-12 17:32:01 +1300

    Added Bank Gem Piles
    They do nothing yet. Trying to work out how to change the components on the fly

---
+++ Splendid/Assets/Contollers/SpriteController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    /// <summary>
+    /// Sets up Visual Layout of game and links these visuals to the model
+    /// </summary>
+public class SpriteController : MonoBehaviour {
+
+    public Sprite Diamond_Sprite;
+    public Sprite Emerald_Sprite;
+    public Sprite Gold_Sprite;
+    public Sprite Onyx_Sprite;
+    public Sprite Ruby_Sprite;
+    public Sprite Sapphire_Sprite;
+
+    GameObject bankPanel;
+    GameObject gemPilePanel;
+    GameObject gemSlot;
+
+    int maxNumOfGems = 7;
+   
+
+    Dictionary<GemPile, GameObject> gemPile_to_GameObjectMap;
+
+    // Use this for initialization
+    void Start () {
+
+
+        //FIXME: testing ablity to change sprites in Canvas
+
+        //Sprite sr = GameObject.Find("Canvas/Bank_Panel/GoldPile/GoldPileImage_0").GetComponent<Sprite>();
+        //sr = Ruby_Sprite;
+
+
+
+
+        ////// Each gemPile will be linked to a GameObject that visually represents it
+        ////gemPile_to_GameObjectMap = new Dictionary<GemPile, GameObject>();
+
+        ////// Make the bank panel, which holds all gempiles
+        ////bankPanel = new GameObject("Bank_Panel");
+        ////bankPanel.transform.position = new Vector3(0, 0, 0);
+        ////bankPanel.transform.SetParent(this.transform, true);
+
+        //////Add in gempiles as children to the bank panel
+        ////gemPilePanel = new GameObject("GemPile_Panel");
+        ////gemPilePanel.transform.position = new Vector3(0, 0, 0);
+        ////gemPilePanel.transform.SetParent(bankPanel.transform, true);
+
+        //////TODO: add button capalbity - click on a gempile should result in gems moving around 
+        ////// Could stay as UI and put this all in a canvas, or have these have a set location out of UI, but that requires checking where the mouse is manually.. ew
+        //////gemPilePanel.AddComponent<Bu>
+
+        //////Add in gemslots as children
+        ////for (int i = 0; i < maxNumOfGems; i++) {
+        ////    gemSlot = new GameObject("GemSlot_Sprite_" + i);
+
+
+        ////    SpriteRenderer sr = gemSlot.AddComponent<SpriteRenderer>();
+        ////    sr.sprite = Gold_Sprite;
+
+        ////    gemSlot.transform.localScale = new Vector2(0.5f, 0.5f);
+        ////    gemSlot.transform.position = new Vector3(sr.sprite.bounds.size.x * i * 0.5f, 0, 0);
+        ////    gemSlot.transform.SetParent(gemPilePanel.transform, true);
+
+
+        ////}
+
+
+
+        //sr.sortingLayerName = "Tiles"; // set a sorting layer?
+
+
+
+
+
+        //    GameObject newCanvas = new GameObject("Canvas");
+        //Canvas c = newCanvas.AddComponent<Canvas>();
+        //c.renderMode = RenderMode.ScreenSpaceOverlay;
+        //newCanvas.AddComponent<CanvasScaler>();
+        //newCanvas.AddComponent<GraphicRaycaster>();
+        //GameObject panel = new GameObject("Panel");
+        //panel.AddComponent<CanvasRenderer>();
+        //Image i = panel.AddComponent<Image>();
+        //i.color = Color.red;
+        //panel.transform.SetParent(newCanvas.transform, false);
+
+
+    }
+	
+	
+}
